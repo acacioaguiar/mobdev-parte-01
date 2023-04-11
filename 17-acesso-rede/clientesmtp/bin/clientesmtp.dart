2b3d2ab60bdd836743147719865802874a5cb7b7
acacioaguiar <acacio1986@yahoo.com.br>
2023-04-11 19:49:36 -0300

    SMTP

--- 17-acesso-rede/clientesmtp/bin/clientesmtp.dart
+++ 17-acesso-rede/clientesmtp/bin/clientesmtp.dart
@@ -1,5 +1,30 @@
 import 'package:clientesmtp/clientesmtp.dart' as clientesmtp;
 
-void main(List<String> arguments) {
-  print('Hello world: ${clientesmtp.calculate()}!');
+
+
+import 'dart:io';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+main() async {
+  // Configura as credenciais SMTP do Gmail
+  final smtpServer = gmail('cezaraguiar5@gmail.com', 'abcd=1234');
+
+  // Cria uma mensagem de e-mail
+  final message = Message()
+    ..from = Address('cezaraguiar5@gmail.com', 'Acacio')
+    ..recipients.add('acacio6891@gmail.com')
+    ..subject = 'teste'
+    ..text = 'teste';
+
+  try {
+    // Envia o e-mail usando o servidor SMTP do Gmail
+    final sendReport = await send(message, smtpServer);
+
+    // Exibe o resultado do envio do e-mail
+    print('E-mail enviado: ${sendReport.mail}');
+  } on MailerException catch (e) {
+    // Exibe informações sobre erros de envio de e-mail
+    print('Erro ao enviar e-mail: ${e.toString()}');
+  }
 }
